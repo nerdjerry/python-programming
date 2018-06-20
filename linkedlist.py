511ec2b14e3bd01fc65561d859419e78436cf951
lucifer <psinghal4210@gmail.com>
2018-06-20 23:31:36 +0530

    Create a linked list with add and print

---
+++ linkedlist.py
@@ -0,0 +1,29 @@
+class node(object):
+    def __init__(self,value,next=None):
+        self.value = value
+        self.next = next
+
+class linked_list(object):
+    def __init__(self):
+        self.head = None
+
+    def add(self,value):
+        current = self.head
+        if not current:
+            self.head = node(value)
+        else:
+            while current.next:
+                current = current.next
+            current.next = node(value)
+    
+    def print_list(self):
+        current = self.head
+        while current:
+            print current.value
+            current = current.next
+
+list = linked_list()
+list.add(4)
+list.add(5)
+list.add(5)
+list.print_list()
