3143f79a98b5a852b2c743237b692ee593fcb22b
lucifer <psinghal4210@gmail.com>
2018-06-24 15:26:58 +0530

    Make a queue using array

---
+++ queue_array.py
@@ -0,0 +1,34 @@
+from error import error
+
+class queue(object):
+
+    def __init__(self):
+        self.insert = -1
+        self.delete = -1
+        self.arr = [None] * 10
+    
+    def enqueue(self,value):
+        if not self.isFull():
+            self.insert +=1
+            self.arr[self.insert] = value
+            return True
+        else:
+            return error("List is full")
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            self.delete +=1
+            result = self.arr[self.delete]
+            self.arr[self.delete] = None
+            return result
+        else:
+            return error("List is empty")
+    
+    def isFull(self):
+        return self.insert == len(self.arr)-1
+    
+    def isEmpty(self):
+        return self.insert == self.delete
+    
+    def print_queue(self):
+        return self.arr
