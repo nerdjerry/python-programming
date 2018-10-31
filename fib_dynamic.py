193bbe500db6e9b493f9efc827b7e735caee1c0a
lucifer <psinghal4210@gmail.com>
2018-11-01 00:43:56 +0530

    Dynamic Programming: Fibonnaci

---
+++ fib_dynamic.py
@@ -0,0 +1,15 @@
+#Fibonnaci number using dynamic programming
+
+def fib(n, mem):
+    if mem[n] != None :
+        return mem[n]
+    if n == 1 or n == 2:
+        result = 1
+    else:
+        result = fib(n-1,mem) + fib (n-2,mem)
+        mem[n] = result
+    return result
+
+n = 310
+mem = [None] * (n+1)
+print(fib(n,mem))
