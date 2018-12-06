523e73d830facbcbc8ac09f5327d5f160f2286df
lucifer <psinghal4210@gmail.com>
2018-12-07 01:16:58 +0530

    Stable count sort

---
+++ array/sort/stableCountSort.py
@@ -0,0 +1,21 @@
+def countSort(array, min, max):
+    length = max - min + 1
+    countArray = [0] * length
+    for i in range(len(array)):
+        countArray[array[i] - min]+=1
+    
+    for i in range (1,length):
+        countArray[i] += countArray[i-1]
+    
+    temp = [None] * len(array)
+    for i in range(len(array)-1, -1, -1):
+        temp[countArray[array[i]-min]-1] = array[i]
+        countArray[array[i]-min]-=1
+    
+    return temp
+
+test = [2,9,2,4,2,6,5,9,3]
+test = countSort(test,2,9)
+
+for i in test:
+    print(i)
