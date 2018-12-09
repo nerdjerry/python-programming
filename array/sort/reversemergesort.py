109a35909c044897d4414896d5e34d0ef1143dd6
lucifer <psinghal4210@gmail.com>
2018-12-10 01:11:47 +0530

    Merge Sort: Reverse Implmentation

--- array/sort/reversemergesort.py
+++ array/sort/reversemergesort.py
@@ -1,19 +1,19 @@
 def mergeSort(array, start, end):
-    if start < end:
+    if not end - start < 2:
         mid = (start+end)//2
         mergeSort(array,start, mid)
-        mergeSort(array,mid+1,end)
+        mergeSort(array,mid,end)
         merge(array,start,mid,end)
 
 def merge(array, start, mid, end):
-    if array[mid] >=  array[mid+1]:
+    if array[mid-1] >=  array[mid]:
         return
-    tempLength = end - start + 1
+    tempLength = end - start
     i = start
-    j = mid+1
+    j = mid
     tindex = 0
     temp = [None] * tempLength
-    while i < mid + 1 and j < end + 1:
+    while i < mid and j < end :
         if array[i] >= array[j]:
             temp[tindex] = array[i]
             tindex +=1
@@ -23,7 +23,7 @@ def merge(array, start, mid, end):
             tindex +=1
             j +=1
     
-    arrayCopy(array,array,i, start+tindex, mid - i +1)
+    arrayCopy(array,array,i, start+tindex, mid - i)
     arrayCopy(temp,array,0,start, tindex)
 
 def arrayCopy(source, dest, sourceStart, destStart, count):
