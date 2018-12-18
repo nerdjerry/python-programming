7381b2add32a60b9424cb697c56f9a0d6ed8e161
lucifer <psinghal4210@gmail.com>
2018-12-18 21:46:27 +0530

    Radix Sort Alpha: Reduce code by using maths to get index instead of dictionary

--- array/sort/radixsortalpha.py
+++ array/sort/radixsortalpha.py
@@ -1,45 +1,19 @@
-dictionary = {
-        'a' : 0,
-        'b' : 1,
-        'c' : 2,
-        'd' : 3,
-        'e' : 4,
-        'f' : 5,
-        'g' : 6,
-        'h' : 7,
-        'i' : 8,
-        'j' : 9,
-        'k' : 10,
-        'l' : 11,
-        'm' : 12,
-        'n' : 13,
-        'o' : 14,
-        'p' : 15,
-        'q' : 16,
-        'r' : 17,
-        's' : 18,
-        't' : 19,
-        'u' : 20,
-        'v' : 21,
-        'w' : 22,
-        'x' : 23,
-        'y' : 24,
-        'z' : 25,
-    }
+def getIndex(letter, position):
+    return ord(letter[position]) - ord('a')
 def radixSort(array, width, radix):
-    for i in range(width):
-        sort(array,i,radix,width)
+    for i in range(width-1, -1, -1):
+        sort(array,i,radix)
 
-def sort(array, position, radix,width):
+def sort(array, position, radix):
     count = [0] * radix
     temp = [None] * len(array)
     for i in range(len(array)):
-        count[dictionary[array[i][width - position - 1]]] += 1
+        count[getIndex(array[i],position)] += 1
     for i in range(1,len(count)):
         count[i] += count[i-1]
     for i in range(len(array)-1, -1, -1):
-        temp[count[dictionary[array[i][width - position - 1]]] - 1] = array[i]
-        count[dictionary[array[i][width - position - 1]]] -= 1
+        temp[count[getIndex(array[i],position)] - 1] = array[i]
+        count[getIndex(array[i],position)] -= 1
     for i in range(len(array)):
         array[i] = temp[i]
 
