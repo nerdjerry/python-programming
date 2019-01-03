15468db9847874db4eaf257447ed396f4f987ecb
lucifer <psinghal4210@gmail.com>
2019-01-04 01:07:04 +0530

    Hashtable: Add method using LInear Probing

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,19 +1,29 @@
 class hashtable(object):
 
     def __init__(self):
-        self.table = [None] * 20
+        self.capacity = 20
+        self.table = [None] * self.capacity
 
     def put(self,key,value):
         hashedKey = self.hash(key)
-        if self.table[hashedKey] != None:
-            print("Cant't insert collision occured")
+        if self.occupied(hashedKey):
+            initialKey = hashedKey
+            hashedKey = (hashedKey + 1) % self.capacity
+            while self.occupied(hashedKey) and hashedKey != initialKey:
+                hashedKey = (hashedKey + 1) % self.capacity
+        if self.occupied(hashedKey):
+            print("Can't insert element as there is no place")
+            #you can resize table here
         self.table[hashedKey] = value
     
     def get(self,key):
         return self.table[self.hash(key)]
 
     def hash(self,key):
-        return len(key) % 20
+        return len(key) % self.capacity
+    
+    def occupied(self, key):
+        return self.table[key] != None
 
 ht = hashtable()
 ht.put("Prateek",34)
