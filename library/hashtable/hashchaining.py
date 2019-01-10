6824f6f1e22a4702d9b63d123705c48e9cbf9b60
lucifer <psinghal4210@gmail.com>
2019-01-10 21:23:22 +0530

    HashTable : Chaining for collision Sibling package import

---
+++ library/hashtable/hashchaining.py
@@ -0,0 +1,45 @@
+from ..linkedlist.DoublyLinkedList import DoublyLinkedList
+from .storedValue import storedValue
+
+class hashchaining(object):
+
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.table = [None] * self.capacity
+    
+    def put(self,key,value):
+        hashedKey = self.hashkey(key)
+        if self.table[hashedKey] == None : 
+            self.table[hashedKey] = DoublyLinkedList()
+        self.table[hashedKey].insertAtStart(storedValue(key,value))
+
+    def get(self,key):
+        hashedKey = self.hashkey(key)
+        current = self.table[hashedKey].head
+        while current != None:
+            if current.getEmployee().key == key:
+                return current.getEmployee().value
+            current = current.getNext()
+        return None
+    
+    def remove(self,key):
+        hashedKey = self.hashkey(key)
+        current = self.table[hashedKey].head
+        removed = None
+        index = 0
+        while current != None:
+            if current.getEmployee().key == key:
+                removed = current.getEmployee().value
+                self.table[hashedKey].remove(index)
+                break
+            index += 1
+            current = current.getNext()
+        return  removed
+    
+    def hashkey(self,key):
+        return len(key) % self.capacity
+
+    def print(self):
+        for i in range(self.capacity):
+            if self.table[i] != None:
+                self.table[i].print()
--- library/hashtable/storedValue.py
+++ library/hashtable/storedValue.py
@@ -3,3 +3,6 @@ class storedValue(object):
     def __init__(self,key,value):
         self.key = key
         self.value = value
+    
+    def __str__(self):
+        return "Key: {} , Value: {}".format(self.key,self.value)
