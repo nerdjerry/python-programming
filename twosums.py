e752242bc46b36adf426d9ed99d01e055b2395af
lucifer <psinghal4210@gmail.com>
2020-11-18 00:04:11 +0530

    Two Sums: Solved Collision and Java code

--- twosums.py
+++ twosums.py
@@ -2,10 +2,10 @@ class Solution:
     def twoSum(self, nums, target):
         hashmap = {}
         for i in range(0, len(nums)):
-            hashmap[nums[i]] = i
             compliment = target - nums[i]
             if compliment in hashmap.keys() and hashmap[compliment] != i:
                 return [i, hashmap[compliment]]
+            hashmap[nums[i]] = i
 
 sol = Solution()
 print(sol.twoSum([3,3],6))
