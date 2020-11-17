1180fb91a81bc6c08db09791965832c982a38f4d
lucifer <psinghal4210@gmail.com>
2020-11-17 23:26:00 +0530

    Two Sums: Solutions Set

--- twosums.py
+++ twosums.py
@@ -1,6 +1,12 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+    def twoSum(self, nums, target):
+        numsInverse = []
+        for a in nums:
+            numsInverse.append(target-a)
+        numsSet = set(nums)
+        numsInverseSet = set(numsInverse)
+        intersectSet = numsSet.intersection(numsInverseSet)
+        return list(intersectSet)
+
+sol = Solution()
+print(sol.twoSum([2,7,11,15],9))
